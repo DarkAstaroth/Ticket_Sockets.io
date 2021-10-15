006e87c3558e3de8ab72900220774c2645357eda
Manuel <mninahuanca4@gmail.com>
2021-10-14 20:46:37 -0400

    Socket : Atender Ticket

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,6 +1,8 @@
 // referencias HTML
 const lblEscritorio = document.querySelector("h1");
 const btnAtender = document.querySelector("button");
+const lblTicket = document.querySelector("small");
+const divAlerta = document.querySelector(".alert");
 
 const searchParams = new URLSearchParams(window.location.search);
 
@@ -11,6 +13,8 @@ if (!searchParams.has("escritorio")) {
 
 const escritorio = searchParams.get("escritorio");
 lblEscritorio.innerText = escritorio;
+
+divAlerta.style.display = "none";
 
 const socket = io();
 
@@ -29,7 +33,12 @@ socket.on("ultimo-ticket", (ultimo) => {
 });
 
 btnAtender.addEventListener("click", () => {
-  socket.emit("siguiente-ticket", null, (ticket) => {
-    lblNuevoTicket.innerText = ticket;
+  socket.emit("atender-ticket", { escritorio }, ({ ok, ticket }) => {
+    if (!ok) {
+      lblTicket.innerText = "Nadie";
+      return (divAlerta.style.display = "");
+    }
+
+    lblTicket.innerText = "Ticket" + ticket.numero;
   });
 });
